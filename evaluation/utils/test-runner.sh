b2ac77f2cdd29dcd44e76bed0793233281314988
Tamas Levai <levait@tmit.bme.hu>
2024-04-15 20:15:42 +0200

    utils/test-runner.sh: Enable net device config

--- evaluation/utils/test-runner.sh
+++ evaluation/utils/test-runner.sh
@@ -6,10 +6,11 @@ VM_IP=${VMIP:-"192.168.122.74"}
 MRF_ENABLE=${MRFENABLE:-"0"}
 NFT_RULES_FILE=${NFTRULESFILE:-""}
 NUM_CORES=${CORES:-"1"}
+MEAS_DEV=${MEASDEV:-"enp7s0np0"}
 
 ssh root@$VM_IP "bash -s" <<EOF
 export MRFENABLE=$MRF_ENABLE
 export NFTRULESFILE=$NFT_RULES_FILE
 /root/nft-file.sh
-ethtool -L enp7s0 combined $NUM_CORES
+ethtool -L $MEAS_DEV combined $NUM_CORES
 EOF
