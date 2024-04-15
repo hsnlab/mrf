7a26685e2afa9dc0c0a2b173fc3362a8585fd19e
Tamas Levai <levait@tmit.bme.hu>
2024-04-15 20:14:43 +0200

    Fix tipsy/scripts/copy-pcap.sh

--- evaluation/tipsy/scripts/copy-pcap.sh
+++ evaluation/tipsy/scripts/copy-pcap.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-
 # Copy PCAP to measurement directory and rewrite destination MAC addresses
+set -x
 
-
-PCAP_FILE=`jq .sut.environ.PCAPFILE`
-MAC_ADDR=`jq .sut.environ.TESTERMAC`
+PCAP_FILE=`jq .sut.environ.PCAPFILE benchmark.json`
+MAC_ADDR=`jq .sut.environ.TESTERMAC benchmark.json`
 
 cp $PCAP_FILE /tmp/tmp.pcap
 tcprewrite --enet-dmac=$MAC_ADDR -i /tmp/tmp.pcap -o traffic.pcap
