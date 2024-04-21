c614f60241ed4fa2891c1f349631c035ce4e4de0
Tamas Levai <levait@tmit.bme.hu>
2024-04-21 15:29:04 +0200

    Make NIC rx-flow-hash configurable
    Default: hash on 5-tuple

--- evaluation/utils/test-runner.sh
+++ evaluation/utils/test-runner.sh
@@ -7,10 +7,13 @@ MRF_ENABLE=${MRFENABLE:-"0"}
 NFT_RULES_FILE=${NFTRULESFILE:-""}
 NUM_CORES=${CORES:-"1"}
 MEAS_DEV=${MEASDEV:-"enp7s0np0"}
+RX_FLOW_HASH=${RXFLOWHASH:-"sdfn"}
 
 ssh root@$VM_IP "bash -s" <<EOF
 export MRFENABLE=$MRF_ENABLE
 export NFTRULESFILE=$NFT_RULES_FILE
 /root/nft-file.sh
 ethtool -L $MEAS_DEV combined $NUM_CORES
+ethtool --config-nfc $MEAS_DEV rx-flow-hash udp4 $RX_FLOW_HASH
+ethtool --config-nfc $MEAS_DEV rx-flow-hash tcp4 $RX_FLOW_HASH
 EOF
