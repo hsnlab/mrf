2b4f8d70dcfb53bf307cbe50fba3f726b23905b2
Tamas Levai <levait@tmit.bme.hu>
2024-04-27 11:29:47 +0200

    utils: Add dep chain length param to ruleset gen

--- evaluation/utils/generate-nft-rules-deps.sh
+++ evaluation/utils/generate-nft-rules-deps.sh
@@ -2,22 +2,36 @@
 set -e
 
 if [ $1 == "-h" ]; then
-    echo "usage: $0 <NUMRULES> <OUTFILE>"
+    echo "usage: $0 <NUMRULES> <OUTFILE> <DEPENDENCY_CHAIN_LEN>"
     exit 0
 fi
 
 NUMRULES=${1:-"2000"}
 OUTFILE=${2:-"/dev/stdout"}
+DEPCHAINLEN=${3:-"1"}
 BASEPORT=500
+
+prefixes="32"
+case $DEPCHAINLEN in
+    2)
+	prefixes="32 0"
+	;;
+    5)
+	prefixes="32 24 16 8 0"
+	;;
+    9)
+	prefixes="32 28 24 20 16 12 8 4 0"
+	;;
+    *)
+	echo "DEPENDENCY_CHAIN_LEN not set, fallback to 1"
+	;;
+esac
 
 echo "add table test" > $OUTFILE
 echo "add chain test test_chain { type filter hook forward priority 0; }" >> $OUTFILE
 
 for i in $(seq $BASEPORT $(expr $(expr $BASEPORT - 1) + $NUMRULES)); do
-    # for n in 32 28 24 20 16 12 8 4 0; do
-    # for n in 32 24 16 8 0; do
-    # for n in 32 0; do
-    # for n in 32; do
+     for n in $prefixes; do
 	echo "add rule test test_chain ip daddr 1.2.3.4/$n udp dport $i counter accept" >> $OUTFILE
     done
 done
